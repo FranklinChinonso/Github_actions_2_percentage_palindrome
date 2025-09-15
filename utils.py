27c3e13d4ef69c9df49b7cfb8ddca8257e11255e
franklinosuji2-afk <franklin.osuji2@gmail.com>
2025-09-15 11:20:01 +0200

    1st workflow

---
+++ utils.py
@@ -0,0 +1,12 @@
+def percentage_caculator(percent, whole):
+    if whole == 0:
+        return 0
+    else:
+        return (percent/100) * whole
+    
+def is_palindrome(s): 
+    #evilisanameofafoemanasilive.
+    #Evil is a name of a foeman as I live. 
+     word = s.replace(" ","")
+     word = word.lower()
+     return word == word[::-1]   
